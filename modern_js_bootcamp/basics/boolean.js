fc895ffcf8b3126e26046184094cceb5399f0fad
Bartosz Ciesielczyk <ext-bartosz.ciesielczyk@here.com>
2020-09-15 13:06:54 +0200

    booleans, comparisons, if statements

---
+++ modern_js_bootcamp/basics/boolean.js
@@ -0,0 +1,12 @@
+let temp = 31
+let isFreezing = temp === 32
+
+let age = 65
+
+if (age <= 7) {
+    console.log("Child discount!")
+}
+
+if (age >= 65) {
+    console.log("Senior discount!");
+}
---
+++ modern_js_bootcamp/basics/temp-conversion.js
@@ -0,0 +1,11 @@
+let fahrenheit = 32
+
+// Calculate celsius and store in celsius variable
+// print that value
+let celsius = (fahrenheit - 32) * 5/9
+console.log(celsius)
+
+// calculate kelvin value and store in variable
+// print that value
+let kelvin = (fahrenheit + 459.67) * 5/9
+console.log(kelvin)
